eb903211451b3c472ee687ef5226d4afaeecb09f
hray3182@gmail.com <hray3182@gmail.com>
2025-03-18 14:57:37 +0800

    1

--- smoke/test.py
+++ smoke/test.py
@@ -1,7 +1,7 @@
 import time
 import RPi.GPIO as GPIO
 SENSOR_PIN = 38
-LED_PIN = 13
+LED_PIN = 40
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(SENSOR_PIN, GPIO.IN)
@@ -11,9 +11,10 @@ while True:
     value = GPIO.input(SENSOR_PIN)
     print(f"gas value = {value}")
     if value < 1: #low active
-        # GPIO.output(LED_PIN, 1)
-        print("LOVW ")
+        GPIO.output(LED_PIN, 1)
+        print("LOW ")
+        print("gas detected")
     else:
-        # GPIO.output(LED_PIN, 0)
+        GPIO.output(LED_PIN, 0)
         print("HIGH")
         time.sleep(0.5)
